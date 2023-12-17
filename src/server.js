3d09978f4800f70df12b2010cd0d036ae9562dab
Hamdhan Ahamed <ahamedhamthan7777@gmail.com>
2023-12-17 23:26:12 +0530

    adjusted  configs port

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const db = require('./config/database'); // Import the database connection module
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(passport.initialize());
