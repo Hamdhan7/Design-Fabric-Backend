57891e77689e65cb743ee77b14189ca8a1d50fc3
Hamdhan Ahamed <ahamedhamthan7777@gmail.com>
2023-12-20 00:27:59 +0530

    in database.js mysql2 changed to mysql2/promise

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,18 @@
-const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10, // Adjust according to your requirements
+    queueLimit: 0
 });
 
-module.exports = connection;
+const promisePool = pool.promise();
+
+module.exports = promisePool;
