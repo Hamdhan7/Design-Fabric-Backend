0e5727d7cb6eac01eb3a2be79dc5c94296c85045
Hamdhan Ahamed <ahamedhamthan7777@gmail.com>
2023-12-20 00:25:43 +0530

    in database.js mysql2 changed to mysql2/promise

--- src/routes/userRoots.js
+++ src/routes/userRoots.js
@@ -7,7 +7,7 @@ const db = require('../config/database');
 // Get all products with image URLs
 router.get('/products', async (req, res) => {
   try {
-      const [results, fields] = await db.execute('SELECT ProductID, Name, Description, Price, ImageURL FROM Product');
+      const [results, fields] = await db.query('SELECT ProductID, Name, Description, Price, ImageURL FROM Product');
       res.json(results);
   } catch (err) {
       console.error('Error executing query:', err);
